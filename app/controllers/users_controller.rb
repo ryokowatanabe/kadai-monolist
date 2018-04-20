ac3ec7e9ac8e8ccf6d85586bc2efd39986ab4a03
rw <mjbjb032@gmail.com>
2018-04-20 08:58:27 +0000

    for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @items = @user.items.uniq
     @count_want = @user.want_items.count
+    @count_have = @user.have_items.count
+
   end  
   
   def create
@@ -23,17 +25,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
-    else
-      flash.now[:danger] = 'ユーザの登録に失敗しました。'
-      render :new
-    end
-  end
   
   private
 
